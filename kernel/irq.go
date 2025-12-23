54b084eaa41a6181f26992407be0cd24ee867b75
Davide Marro <davideraffaele.marro@gmail.com>
2025-12-23 16:19:13 +0000

    Removing Print of the ticks

--- kernel/irq.go
+++ kernel/irq.go
@@ -2,18 +2,12 @@ package kernel
 
 import (
 	"github.com/dmarro89/go-dav-os/keyboard"
-	"github.com/dmarro89/go-dav-os/terminal"
 )
 
 var ticks uint32
 
 func IRQ0Handler() {
 	ticks++
-
-	if ticks%100 == 0 {
-		terminal.Print(".")
-	}
-
 	PICEOI(0)
 }
 
