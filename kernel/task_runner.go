01d042690ded4e47951af30db9492b077d8fce20
Davide <davideraffaele.marro@gmail.com>
2026-02-09 11:27:44 +0100

    Fixing build issue

--- kernel/task_runner.go
+++ kernel/task_runner.go
@@ -2,51 +2,31 @@ package kernel
 
 import "github.com/dmarro89/go-dav-os/kernel/scheduler"
 
-type builtInProgram struct {
-	name    [16]byte
-	nameLen int
-	entry   func()
-}
-
 var helloProgramMsg = [...]byte{
 	'H', 'e', 'l', 'l', 'o', ' ', 'f', 'r', 'o', 'm', ' ',
 	't', 'a', 's', 'k', '\n',
 }
 
-var builtInPrograms = [...]builtInProgram{
-	{
-		name:    [16]byte{'h', 'e', 'l', 'l', 'o'},
-		nameLen: 5,
-		entry:   programHello,
-	},
-}
+var helloProgramName = [...]byte{'h', 'e', 'l', 'l', 'o'}
+
+const helloProgramNameLen = 5
 
 func RunProgram(name *[16]byte, nameLen int) (pid int, ok bool) {
-	for i := 0; i < len(builtInPrograms); i++ {
-		p := &builtInPrograms[i]
-		if p.nameLen != nameLen {
-			continue
-		}
-
-		match := true
-		for j := 0; j < nameLen; j++ {
-			if p.name[j] != name[j] {
-				match = false
-				break
-			}
-		}
-		if !match {
-			continue
-		}
-
-		task := scheduler.NewTask(p.entry)
-		if task == nil {
-			return -1, false
-		}
-		return task.ID, true
+	if nameLen != helloProgramNameLen {
+		return -1, false
 	}
 
-	return -1, false
+	for i := 0; i < helloProgramNameLen; i++ {
+		if name[i] != helloProgramName[i] {
+			return -1, false
+		}
+	}
+
+	task := scheduler.NewTask(programHello)
+	if task == nil {
+		return -1, false
+	}
+	return task.ID, true
 }
 
 func programHello() {
