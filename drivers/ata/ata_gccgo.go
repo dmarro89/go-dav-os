2747431e55b14911661f8242ae6ae9beb823f5c0
Deep-Dey1 <deepdey715@gmail.com>
2026-02-23 15:02:18 +0530

    feat: add comprehensive unit tests for shell parsing helpers
    Run: go test ./shell -v

---
+++ drivers/ata/ata_gccgo.go
@@ -0,0 +1,10 @@
+//go:build gccgo
+
+package ata
+
+func inb(port uint16) byte
+func outb(port uint16, value byte)
+func insw(port uint16, addr *byte, count int)
+func outsw(port uint16, addr *byte, count int)
+
+
---
+++ terminal/terminal_gccgo.go
@@ -0,0 +1,8 @@
+//go:build gccgo
+
+package terminal
+
+func outb(port uint16, value byte)
+func debugChar(c byte)
+
+
