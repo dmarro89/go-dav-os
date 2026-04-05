2a6121de721ae71a1fd7f9b4281da29f30cc66ee
Deepak Singh <deepakupkgs@gmail.com>
2026-04-05 08:03:41 +0000

    feat: add examples folder with build and run scripts (closes #112)

---
+++ examples/run_docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# examples/run_docker.sh
+# Build and run the OS using the Docker-based toolchain.
+
+# Build and run through Docker
+echo "Building and running the OS inside Docker toolchain..."
+make docker-run
